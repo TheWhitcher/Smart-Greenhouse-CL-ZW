65c7cab962d4d6705fb2f5c2287bf7c0cd22bb51
TheWhitcher <zwhitcher1995@gmail.com>
2023-11-24 09:12:20 -0500

    Removed the code relating to the GUI that was removed.

--- main.py
+++ main.py
@@ -4,9 +4,6 @@
 # Version:  1.0
 
 # Imports
-from tkinter import *
-import os
-import GreenHouseGUI
 import temperature_monitor
 import uv_light_monitor
 import soil_moisture_monitor
@@ -15,6 +12,12 @@ import threading
 
 # Global Variables
 water_cooldown = 0
+max_temp = 0
+min_temp = 0
+max_moisture = 0
+min_moisture = 0
+max_uv = 0
+min_uv = 0
 
 # Setup the Components
 def setup():
@@ -22,37 +25,6 @@ def setup():
 	soil_moisture_monitor.setup()
 	temperature_monitor.setup()
 	uv_light_monitor.setup()
-	
-	# Setup GUI
-	def on_closing():
-		app.master.withdraw()
-        
-	global app
-	root=Tk()
-	root.title('Greenhouse GUI')
-	root.geometry('430x170')
-	app = GreenHouseGUI.Application(root)
-	
-	root.protocol("WM_DELETE_WINDOW", on_closing)
-	
-	# Default values
-	global max_temp
-	max_temp = float(app.tempMax.cget("text"))
-	
-	global min_temp
-	min_temp = float(app.tempMin.cget("text"))
-	
-	global max_moisture
-	max_moisture = float(app.soilMax.cget("text"))
-	
-	global min_moisture
-	min_moisture = float(app.soilMin.cget("text"))
-	
-	global max_uv
-	max_uv = float(app.uvMax.cget("text"))
-	
-	global min_uv
-	min_uv = float(app.uvMin.cget("text"))
 	
 # Main Loop every 1 second
 def loop():
@@ -77,44 +49,14 @@ def loop():
 		print("| Light	     " + str(min_uv) + "     " + str(max_uv) + "    " + str(light))
 		print("+-----------------------------------------------+")
 		
-		# Update GUI Labels
-		app.tempActual.config(text=str(temperature))
-		app.soilActual.config(text=str(moisture))
-		app.uvActual.config(text=str(light))
-		
-		# Detemine GUI Label colors
-		actualFG(app.tempActual, temperature, max_temp, min_temp)
-		actualFG(app.soilActual, moisture, max_moisture, min_moisture)
-		actualFG(app.uvActual, light, max_uv, min_uv)
-		
-		# Update GUI
-		app.update_idletasks()
-		app.update()
-		#app.mainloop()
-		
-		# Update values
-		max_temp = float(app.tempMax.cget("text"))
-		min_temp = float(app.tempMin.cget("text"))
-		max_moisture = float(app.soilMax.cget("text"))
-		min_moisture = float(app.soilMin.cget("text"))
-		max_uv = float(app.uvMax.cget("text"))
-		min_uv = float(app.uvMin.cget("text"))
-		
 		# Run the sprinkler for 10 seconds
 		if(moisture <= min_moisture and (time.monotonic() - water_cooldown) >= 60):
 			# Start a new thread to run the motor concurrently
-			t = threading.Thread(target=soil_moisture_monitor.RunMotor)
-			t.start()
+			water_pump_thread = threading.Thread(target=soil_moisture_monitor.RunMotor)
+			water_pump_thread.start()
 			water_cooldown = time.monotonic()
 			
 		time.sleep(0.5)
-		
-# Change the foreground color of the Actual values
-def actualFG(label, value, max, min):
-	if ( value <= max and value >= min):
-		label.config(fg = "green")
-	else:
-		label.config(fg = "red")
 		
 # Cleans up GPIO
 def destroy():
@@ -129,6 +71,6 @@ try:
 	loop()
 except KeyboardInterrupt: 
 	destroy()
-	print ('The end !')
+	print ('The end!')
 finally:
 	destroy()
